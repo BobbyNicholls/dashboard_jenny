acfacf2f8de8950b7222eda8ca287d2989d3b810
tsameti <robert.r.nicholls@gmail.com>
2021-05-06 22:48:17 +0100

    new main with basic free YF api call

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import yfinance as yf
+
+adbe = yf.Ticker("ADBE")
+# get stock info
+x = adbe.info
+print(x['marketCap'])
+
+"""
+returns:
+{
+ 'quoteType': 'EQUITY',
+ 'quoteSourceName': 'Nasdaq Real Time Price',
+ 'currency': 'USD',
+ 'shortName': 'Microsoft Corporation',
+ 'exchangeTimezoneName': 'America/New_York',
+  ...
+ 'symbol': 'MSFT'
+}
+"""
+
+# get historical market data, here max is 5 years.
+msft.history(period="max")
