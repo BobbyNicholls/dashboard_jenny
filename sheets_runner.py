826c4db9012eab5ea0d5436d1e606ba95245daea
tsameti <robert.r.nicholls@gmail.com>
2022-10-21 19:55:49 +0100

    very simple version of sheet runner now set up

--- sheets_runner.py
+++ sheets_runner.py
@@ -4,19 +4,29 @@ import pandas as pd
 
 from scraping_runner import scrape
 
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = [
     "https://www.googleapis.com/auth/drive",
     "https://www.googleapis.com/auth/spreadsheets",
 ]
-TICKERS = ["NFLX"]
-credentials = Credentials.from_authorized_user_file("token.json", SCOPES)
-gc = gspread.authorize(credentials)
-spreadsheet_key = "1nYYIkdzyH55uP6nAbRbkBJe5sWZPwJc6Z2GJ4O_j2sc"
-worksheet_name = "API Test"
-sheet = gc.open_by_key(spreadsheet_key)
-worksheet = sheet.worksheet("API Test")
-results_df = scrape(TICKERS)
-# worksheet.clear() clears all cells, use this to update a spreadsheet anew with the old one appended from before
-worksheet.update([results_df.columns.values.tolist()] + results_df.values.tolist())
+SPREADSHEET_KEY = "1nYYIkdzyH55uP6nAbRbkBJe5sWZPwJc6Z2GJ4O_j2sc"
+TICKERS = ["MSFT", "GOOG"]
+
+
+def runner():
+    # If modifying these scopes, delete the file token.json.
+    credentials = Credentials.from_authorized_user_file("token.json", SCOPES)
+    gc = gspread.authorize(credentials)
+    worksheet_name = "API Test"
+    sheet = gc.open_by_key(SPREADSHEET_KEY)
+    worksheet = sheet.worksheet(worksheet_name)
+    current_df = pd.DataFrame(worksheet.get_all_records())
+    results_df = scrape(TICKERS)
+    results_df["date"] = str(pd.to_datetime("today").date())
+    new_df = current_df.append(results_df)
+    worksheet.clear()
+    worksheet.update([new_df.columns.values.tolist()] + new_df.values.tolist())
+
+
+if __name__ == "__main__":
+    runner()
+
